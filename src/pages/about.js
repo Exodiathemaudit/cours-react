d1b19955ef1d100ae42fb8c45c9d3de30f7203bb
Exodiathemaudit <moreauchristopher7@gmail.com>
2023-07-12 12:00:49 +0200

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const about = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1>A propos</h1>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Dolore facilis non vel deleniti rem eos in soluta. 
+                Sit maiores sunt alias minima eligendi modi libero laborum, 
+                temporibus magni, delectus suscipit assumenda! 
+                Saepe ipsa excepturi impedit autem quasi! Recusandae, provident. 
+                Quis unde sequi ipsam soluta dolorem maiores, eaque vitae facilis 
+                reprehenderit labore vel iste modi voluptatibus distinctio excepturi assumenda,
+                 ut alias. Ad ratione eveniet, sapiente provident architecto fuga, quasi incidunt 
+                 minus placeat error enim minima harum? Blanditiis magnam, repellat commodi ratione
+                 alias dolorem quia, officia doloremque eligendi veritatis obcaecati totam voluptas! 
+                 Aspernatur labore quos fugiat odit, quibusdam necessitatibus? Quod, velit voluptas?
+            </p>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque ducimus dolorem totam 
+                obcaecati rem est neque nobis vero iusto enim ipsa tempora illo iste excepturi tempore, 
+                eligendi suscipit laboriosam cum. </p>
+        </div>
+        
+    );
+};
+
+export default about;
